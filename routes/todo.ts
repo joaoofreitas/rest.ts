80c450d3b75438d5021c4542aea9052906d626b7
joaoofreitas <joaoofreitas@pm.me>
2021-01-23 19:31:51 +0000

    🚧 Structure Created

---
+++ routes/todo.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+
+const router = express.Router()
+
+//Getting
+router.get('/', async (req, res) => {
+    
+});
+
+// Creating
+router.post('/', async (req, res) => {
+
+});
+
+//Get by ID
+router.get('/:id', async (req, res) => {
+
+});
+
+// Updating by ID
+router.patch('/:id', (req, res) => {
+
+});
+
+// Deleting by ID
+router.delete('/:id', (req, res) => {
+
+});
+
+export default router;
---
+++ server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import todo from './routes/todo'
+import 'dotenv/config'
+
+const dbURL = (process.env.DB_HOST != null) ? process.env.DB_HOST : ''
+mongoose.connect(dbURL, { useUnifiedTopology: true , useNewUrlParser: true})
+.catch((err) => console.error(err))
+
+const app = express()
+const db = mongoose.connection
+const PORT: number = 3000
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+
+app.use('/todo', todo)
+
+app.listen(PORT, () => console.log('Server has just started...'))
